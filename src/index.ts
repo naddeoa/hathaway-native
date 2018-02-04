01a574f2691741c94021e465ea710bab8630a456
Anthony Naddeo <anthony.naddeo@gmail.com>
2018-02-04 11:44:41 -0800

    Updated the start logic to match web stuff

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
-import { AppRegistry, View } from 'react-native';
+import { AppRegistry } from 'react-native';
 import * as React from 'react';
 import { Switchable, Program, Model, RootView, ViewProps } from 'hathaway-core';
 
+export * from 'hathaway-core';
+
 export function start<M extends Model, Msg extends Switchable>(name: string, program: Program<M, Msg>) {
   const Component: React.SFC<ViewProps<M, Msg, {}>> = function () {
-    return (
-      <View>
-        {React.createElement(RootView, { program })}
-      </View>
-    );
+    return React.createElement(RootView, { program });
   };
 
   AppRegistry.registerComponent(name, () => Component);
